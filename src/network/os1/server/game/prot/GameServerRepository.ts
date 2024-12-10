4dbc169fa5345876eb8411454827b4c86c331201
Tyler Bochard <zeruth09@gmail.com>
2024-12-10 04:10:48 -0500

    feat: Logout / PlaySong packets

--- src/network/os1/server/game/prot/GameServerRepository.ts
+++ src/network/os1/server/game/prot/GameServerRepository.ts
@@ -2,6 +2,10 @@ import ServerRepository from '#/network/server/prot/ServerRepository.ts';
 
 import IfOpenTopEncoder from '#/network/os1/server/game/codec/IfOpenTopEncoder.ts';
 import IfOpenTop from '#/network/server/game/model/IfOpenTop.ts';
+import Logout from '#/network/server/game/model/Logout.ts';
+import LogoutEncoder from '../codec/LogoutEncoder.ts';
+import PlaySong from '#/network/server/game/model/PlaySong.ts';
+import PlaySongEncoder from '../codec/PlaySongEncoder.ts';
 import RebuildNormalEncoder from '#/network/os1/server/game/codec/RebuildNormalEncoder.ts';
 import RebuildNormal from '#/network/server/game/model/RebuildNormal.ts';
 
@@ -10,6 +14,8 @@ export default class GameServerRepository extends ServerRepository {
         super();
 
         this.bind(IfOpenTop, new IfOpenTopEncoder());
+        this.bind(Logout, new LogoutEncoder());
+        this.bind(PlaySong, new PlaySongEncoder());
         this.bind(RebuildNormal, new RebuildNormalEncoder());
     }
 }
