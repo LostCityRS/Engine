93b313c52871ba16ce111a1283272bcd7353fab2
Tyler Bochard <zeruth09@gmail.com>
2024-12-20 17:29:35 -0500

    fix: logout -> interfaceClick
    this fixes a silly mistake that acted like all normal interface clicks should logout :X
    
    fix: don't crash on noop interfaces
    
    fix: interface click crash

--- src/network/os1/client/codec/game/InterfaceClickDecoder.ts
+++ src/network/os1/client/codec/game/InterfaceClickDecoder.ts
@@ -3,9 +3,10 @@ import type Packet from '#/io/Packet.ts';
 import type ClientMessage from '#/network/client/ClientMessage.ts';
 import GameClientLimit from '#/network/client/codec/game/GameClientLimit.ts';
 import MessageDecoder from '#/network/client/codec/MessageDecoder.ts';
+import NoOp from '#/network/client/model/game/NoOp.ts';
 import Logout from '#/network/server/model/game/Logout.ts';
 
-export default class LogoutDecoder extends MessageDecoder {
+export default class InterfaceClickDecoder extends MessageDecoder {
     opcode = 155;
     size = 4;
     limit = GameClientLimit.CLIENT;
@@ -13,6 +14,10 @@ export default class LogoutDecoder extends MessageDecoder {
     read(buf: Packet): ClientMessage {
         const iface = buf.g4();
 
-        return new Logout()
+        if (iface === 11927558)
+            return new Logout()
+
+        console.log('iface click: ', iface)
+        return NoOp
     }
 }
---
+++ src/network/os1/client/handler/game/InterfaceClickHandler.ts
@@ -0,0 +1,13 @@
+import type NetworkPlayer from '#/engine/NetworkPlayer.ts';
+import MessageHandler from '#/network/client/handler/MessageHandler.ts';
+import NoOp from '#/network/client/model/game/NoOp.ts';
+import GameServerMessage from '#/network/server/GameServerMessage.ts';
+
+export default class InterfaceClickHandler extends MessageHandler {
+    handle(message: GameServerMessage | NoOp, player: NetworkPlayer): boolean {
+        if (message == NoOp)
+            return true
+        player.write(message as GameServerMessage);
+        return true;
+    }
+}
